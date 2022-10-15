dc2f59d647591e45e3a23ae789fcab5b0f1a4060
Kumar pun <apple@Apples-MacBook-Pro.local>
2022-10-15 23:18:53 +0545

    design basepage and call from test folder

--- AppiumFramework/tests/LoginTest.py
+++ AppiumFramework/tests/LoginTest.py
@@ -1,8 +1,14 @@
 from AppiumFramework.base.DriverClass import Driver
 import AppiumFramework.utilities.CustomLogger as cl
+from AppiumFramework.base.BasePage import BasePage
 
-driver = Driver()
+driver1 = Driver()
 log = cl.customLogger()
 
-driver.getDriverMethod()
+driver = driver1.getDriverMethod()
 log.info("Launching App")
+
+bp = BasePage(driver)
+
+element = bp.waitForElement("//android.widget.Button[@index='3']", "xpath")
+element.click()
